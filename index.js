886c4d4cf9a39c0439c5ca518019190ee34e0094
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-03-01 22:30:57 -0500

    added unit testing modules

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const db = require("./models");
 
 app.use("/api", require("./api"));
 
 app.listen(port, () => {
 	console.log(`Listening on ${port}`);
 });
+
+module.exports = app;
