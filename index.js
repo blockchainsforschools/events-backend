91af6adf281ecd7ef4670755c6126d30437cf915
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-03-02 17:49:25 -0500

    added sessions

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const session = require("express-session");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const {sequelize} = require("./models");
+const sessionStore = new SequelizeStore({db: sequelize});
+
+app.set('trust proxy', 1);
+
+app.use(session({
+	secret: "banana",
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		httpOnly: true
+	},
+	store: sessionStore
+}));
+
+sessionStore.sync();
 
 app.use("/api", require("./api"));
 
