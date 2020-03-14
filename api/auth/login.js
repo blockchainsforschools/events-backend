b17ecbadfce7e63c3119590033ef0df2ba006dc1
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-03-14 14:13:32 -0400

    fixed route for user auth

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,11 +1,26 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const {user} = require("../../database/models");
 
-router.post("/", (req, res) => {
-	// do stuff
-	// post-data can be accessed through req.body.<post-field-name>
+router.post("/", async (req, res) => {
+	const findUser = await user.findOne({
+		where: {
+			email: req.body.email
+		}
+	});
 
-	res.json({
-		success: true
+	if(findUser){
+		const success = await bcrypt.compare(req.body.password, findUser.password);
+
+		if(success){
+			return res.json({
+				success: true
+			});
+		}
+	}
+
+	return res.json({
+		success: false
 	});
 
 });
