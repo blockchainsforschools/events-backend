a36e60295c538d37ba722dd1923e5a770fbf1019
ray. lee <rlee00@stuy.edu>
2020-04-11 17:33:46 -0400

    Add Google OAuth2 to backend

--- api/auth/index.js
+++ api/auth/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/login", require("./login"));
 router.use("/logout", require("./logout"));
+router.use("/oauthlogin", require("./oauthlogin"));
 
 module.exports = router;
---
+++ api/auth/oauthlogin.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const RefusalError = require("../../utils/refusalerror");
+const { OAuth2Client } = require("google-auth-library");
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const client = new OAuth2Client(CLIENT_ID);
+
+const verify = async (token) => {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userId = payload["sub"];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+};
+
+router.post("/", async (req, res) => {
+  verify(req.body.token).catch((error) => {
+    console.error();
+    throw new RefusalError(
+      "Those OAuth credentials were invalid. Please try again.",
+      "INVALID_CREDENTIALS"
+    );
+  });
+});
