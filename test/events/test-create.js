750d84c1951a7c01fa7c731ea143f71ae332f0b1
ray. lee <rlee00@stuy.edu>
2020-05-02 19:43:15 -0400

    Create error tests for POST /api/events/create

--- test/events/test-create.js
+++ test/events/test-create.js
@@ -24,6 +24,32 @@ describe("POST /api/events/create", function () {
 				return done();
 			});
 	});
+	const brokenEvent = {
+		name: "Blockchains4Hacks 2020",
+		startTime: "hello",
+		endTime: "hello",
+		tags: ["hackathon", "microsoft"],
+		imgURL:
+			"https://c.s-microsoft.com/en-us/CMSImages/NYC_flagship_storefront.jpg?version=8db9b1ce-5625-4986-b31d-36112d9160f2"
+	};
+	it("should throw a RefusalError if not all POST information are of the valid type", done => {
+		supertest(app)
+			.post("api/events/create")
+			.send(brokenEvent)
+			.set("Accept", "application", /json/)
+			.expect("Content-Type", /json/)
+			.expect(403)
+			.expect(res => {
+				assert.strictEqual(res.body.success, true);
+			})
+			.end(done);
+	});
+	it("should throw a RefusalError if the URL already exists for another event", done => {
+		// TODO: Do this test when the db connection is established
+	});
+	it("should throw a RefusalError if not all the location provided is not valid.", done => {
+		// TODO: Do this test when the db connection is established
+	});
 });
 
 // test refusalerrors with 403 error code
