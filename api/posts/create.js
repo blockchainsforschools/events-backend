046f6cec52ea76dfb94928120f66a67b3996a02c
ray. lee <rlee00@stuy.edu>
2020-06-06 13:23:17 -0400

    Add /posts/create

---
+++ api/posts/create.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { Posts } = require("../../database/models/posts");
+
+router.post("/", async (req, res) => {
+	const { title, content } = req.body;
+	const allFieldsProvided = title && content;
+
+	if (!allFieldsProvided) {
+		throw new RefusalError(
+			"Not all required POST body information was provided.",
+			"INCOMPLETE_BODY"
+		);
+	}
+
+	const post = await Posts.create({
+		title,
+		content
+	});
+
+	res.status(201).json({
+		success: true,
+		payload: post
+	});
+});
