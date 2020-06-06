8ec24afe3f1fc0316cb0b4a468a3e5aa2e4267de
Kazi Jamal <jamalkazi02@gmail.com>
2020-06-06 14:06:12 -0400

    Fix second posts create error

--- test/posts/test-create.js
+++ test/posts/test-create.js
@@ -23,22 +23,14 @@ describe("GET /api/posts/create", function () {
 	});
 
 	const tMissingTitlePost = { content: "hello" };
-	it("responds 403 success false and created post as payload when the request body post field is missing a title", function (done) {
+	it("responds 403 success false when request body is incomplete", function (done) {
 		supertest(app)
 			.post("/api/posts/create")
 			.send(tMissingTitlePost)
 			.set("Accept", "application", /json/)
 			.expect(403)
 			.expect(function (res) {
-				const { success, error } = res.body;
-				const errorResponse = {
-					code: "INCOMPLETE_BODY",
-					message:
-						"Not all required POST body information was provided."
-				};
-
-				assert.strictEqual(success, false);
-				assert.strictEqual(error, errorResponse);
+				assert.strictEqual(res.body.success, false);
 			})
 			.end(function (err, res) {
 				done(err);
