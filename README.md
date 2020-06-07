a69b283aa45c118caeed0ce798aac18d2af34895
Abir Taheer <abir@taheer.me>
2020-06-06 22:35:48 -0400

    restyled code with prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # events-backend
+
 [![Coverage Status](https://coveralls.io/repos/github/blockchainsforschools/events-backend/badge.svg?branch=master)](https://coveralls.io/github/blockchainsforschools/events-backend?branch=master)
--- database/models/posts.js
+++ database/models/posts.js
@@ -1,11 +1,15 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Posts = sequelize.define('Posts', {
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT
-  }, {});
-  Posts.associate = function(models) {
-    // associations can be defined here
-  };
-  return Posts;
+	const Posts = sequelize.define(
+		"Posts",
+		{
+			title: DataTypes.STRING,
+			content: DataTypes.TEXT
+		},
+		{}
+	);
+	Posts.associate = function (models) {
+		// associations can be defined here
+	};
+	return Posts;
 };
