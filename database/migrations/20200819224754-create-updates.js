eb476e96578a242886a5873274a6e9a0d227dde1
Restyled.io <commits@restyled.io>
2020-08-20 04:06:04 +0000

    Restyled by prettier

--- database/migrations/20200819224754-create-updates.js
+++ database/migrations/20200819224754-create-updates.js
@@ -1,36 +1,36 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Updates', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userID: {
-        type: Sequelize.INTEGER
-      },
-      eventID: {
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      content: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('updates');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable("Updates", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			userID: {
+				type: Sequelize.INTEGER
+			},
+			eventID: {
+				type: Sequelize.INTEGER
+			},
+			title: {
+				type: Sequelize.STRING
+			},
+			content: {
+				type: Sequelize.STRING
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable("updates");
+	}
 };
--- database/models/updates.js
+++ database/models/updates.js
@@ -11,14 +11,14 @@ module.exports = (sequelize, DataTypes) => {
 		{}
 	);
 	Updates.associate = function (models) {
-    // associations can be defined here
-    // an update can belong to only one user
+		// associations can be defined here
+		// an update can belong to only one user
 		Updates.belongsTo(models.Users, {
 			foreignKey: "userID",
 			targetKey: "id"
 		});
 
-    // an update can only be referenced to its parent event
+		// an update can only be referenced to its parent event
 		Updates.belongsTo(models.Events, {
 			foreignKey: "eventID",
 			targetKey: "id"
