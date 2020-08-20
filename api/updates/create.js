82d7b3f33faf64116f763266df303a841e0eaccb
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-08-20 06:17:20 -0400

    Added parameter merging in route handler
    If the merge params option is not set to true, the router cannot access parameters from the parent router

--- api/updates/create.js
+++ api/updates/create.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 const { Updates } = require("../../database/models/updates");
 const RefusalError = require("./../../utils/refusalerror");
 
