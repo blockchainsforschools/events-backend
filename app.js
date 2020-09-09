eba443379f8622c8475ca50c2154c0ae0d8cdca7
Abir Taheer <abir@taheer.me>
2020-09-09 17:07:00 -0400

    added graphql to app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const jwtValidator = require("./middleware/jwtValidator");
+const apolloServer = require("./graphql");
 const { COOKIE_SECRET } = require("./constants");
 
 app.set("trust proxy", 1);
@@ -12,6 +13,8 @@ app.use(cookieParser(COOKIE_SECRET));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(jwtValidator);
+
+apolloServer.applyMiddleware({ app, cors: false, path: "/graphql" });
 
 app.use("/api", require("./api"));
 
